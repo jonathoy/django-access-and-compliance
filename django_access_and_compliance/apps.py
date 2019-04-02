1d689b0eccbe392e73ed3ac9e04ffd4f1f644804
Jon Yu <jonathoy@umich.edu>
2019-04-02 10:17:19 -0400

    Tweak package structure

--- django_access_and_compliance/apps.py
+++ django_access_and_compliance/apps.py
@@ -1,5 +1,10 @@
 from django.apps import AppConfig
-
+from .models import ensure_compliant
+from django.contrib.auth.signals import user_logged_in
 
 class AccessAndComplianceConfig(AppConfig):
     name = 'access_and_compliance'
+    verbose = 'Access and Compliance'
+
+    def ready(self):
+        user_logged_in.connect(ensure_compliant)
--- django_access_and_compliance/models.py
+++ django_access_and_compliance/models.py
@@ -1,7 +1,4 @@
 from django.db import models
-from django.contrib.auth.signals import user_logged_in
 
-def check_access_and_compliance(sender, request, user):
+def ensure_compliant(sender, request, user):
     print('>>>>>>>>>> Logged in!!!!')
-
-user_logged_in.connect(check_access_and_compliance)
