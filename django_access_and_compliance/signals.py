f5ee9f55fc81a866bc87dfa1432729136321e8c6
Jon Yu <jonathoy@umich.edu>
2019-04-16 08:44:04 -0400

    Use logger

--- django_access_and_compliance/signals.py
+++ django_access_and_compliance/signals.py
@@ -1,17 +1,22 @@
 import requests
+import logging
 from django.conf import settings
 from django.contrib.auth.models import Permission
+
+logger = logging.getLogger(__name__)
 
 def ensure_compliant(sender, request, user, **kwargs):
     payload = {'uniqname': user.username}
     response = requests.get(settings.ACCESS_AND_COMPLIANCE_VALIDATION_URL, params=payload)
     response.raise_for_status()
+    permission = Permission.objects.get(codename='confirmed_access_and_compliance')
 
     if _is_compliant(response):
-        permission = Permission.objects.get(codename='confirmed_access_and_compliance')
         user.user_permissions.add(permission)
+        logger.debug(f'{user} has attested to the data access and compliance policy')
     else:
-        print(f'{user} has not attested to data compliance policy!')
+        user.user_permissions.remove(permission)
+        logger.debug(f'{user} has not attested to data compliance policy')
 
 def _is_compliant(response):
     return response.text in settings.ACCESS_AND_COMPLIANCE_TRUTHY_VALUES
