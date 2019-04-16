1b539402fdc158ed1a9e16afa56fc2976facf93d
Jon Yu <jonathoy@umich.edu>
2019-04-16 10:22:45 -0400

    Fix potential Permission issue

--- django_access_and_compliance/signals.py
+++ django_access_and_compliance/signals.py
@@ -1,7 +1,11 @@
 import requests
 import logging
+
 from django.conf import settings
 from django.contrib.auth.models import Permission
+from django.contrib.contenttypes.models import ContentType
+
+from .models import AccessAndCompliance
 
 logger = logging.getLogger(__name__)
 
@@ -9,7 +13,7 @@ def ensure_compliant(sender, request, user, **kwargs):
     payload = {'uniqname': user.username}
     response = requests.get(settings.ACCESS_AND_COMPLIANCE_VALIDATION_URL, params=payload)
     response.raise_for_status()
-    permission = Permission.objects.get(codename='confirmed_access_and_compliance')
+    permission = _get_permission()
 
     if _is_compliant(response):
         user.user_permissions.add(permission)
@@ -17,6 +21,13 @@ def ensure_compliant(sender, request, user, **kwargs):
     else:
         user.user_permissions.remove(permission)
         logger.debug(f'{user} has not attested to data compliance policy')
+
+def _get_permission():
+    content_type = ContentType.objects.get_for_model(AccessAndCompliance)
+    return Permission.objects.get(
+        codename='confirmed_access_and_compliance',
+        content_type=content_type,
+    )
 
 def _is_compliant(response):
     return response.text in settings.ACCESS_AND_COMPLIANCE_TRUTHY_VALUES
